db88241c3836097e95918b15bd7e7092b50366d3
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-31 16:40:08 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,8 @@
 def alphabetize(arr)
   esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_hash = {}
   
-  esp_map = esp_alph.each_char.with_index.to_a.to_h
-  
-  puts esp_map
+  esp_alph.each_ch
 end
 
 
