02545c99e691f8ae5443b08329f1bc0a157e52c2
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-31 17:12:06 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,7 +4,7 @@ def alphabetize(arr)
   
   #esp_map = esp_alph.each_char.with_index.to_a.to_h
   #arr.sort { |a,b| a.length <=> b.length }
-  
+  arr.sort_by{|string| string.tr(esp_alph, ascii)}
   sorted_arr = arr.sort {|a,b| esp_alph.index(a) <=> esp_alph.index(b)}
     puts sorted_arr
 end
