5168ad0e5de2d1e64a2f9b7f53e5281684e5ffbf
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-31 17:01:20 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,8 +4,8 @@ def alphabetize(arr)
   esp_map = esp_alph.each_char.with_index.to_a.to_h
   #arr.sort { |a,b| a.length <=> b.length }
   
-  arr.sort_by {|word| esp_map[word[0]]}
-    
+  sorted_arr = arr.sort_by {|word| esp_map[word[0]]}
+    puts sorted_arr
 end
 
 
