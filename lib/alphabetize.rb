fb2c92e82799f5ffe6089a4401681da567aff2fb
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-31 17:13:19 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,10 +2,8 @@ def alphabetize(arr)
   esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
   
-  sorted_arr = arr.sort_by{|string| string.tr(esp_alph, ascii)}
+arr.sort_by{|string| string.tr(esp_alph, ascii)}
    
-    #puts sorted_arr
 end
 
 
-alphabetize(["ĝuste", "ĝis revido"])
